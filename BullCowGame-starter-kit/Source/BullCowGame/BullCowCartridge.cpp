b93c959166b3b378c1ea7e182c2a915e5c71f07d
Bhanu Prasad M <mbhanuprsd@gmail.com>
2021-08-25 20:18:18 +0530

    49. COding basic game loop

--- BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -4,9 +4,21 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+    PrintLine(TEXT("Welcome to Bull Cow game!"));
+    PrintLine(TEXT("Guess the 4 letter word!")); // Magic number removal
+    PrintLine(TEXT("Press enter to continue..."));
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
-
+    ClearScreen();
+    FString HiddenWord = TEXT("cake");  // Move outside this function
+    if (HiddenWord == Input)
+    {
+        PrintLine(TEXT("You have Won! :)"));
+    }
+    else
+    {
+        PrintLine(TEXT("You have Lost! :("));
+    }
 }
