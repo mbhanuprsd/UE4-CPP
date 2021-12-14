362f1a8a02db4949ae23dc05289afa570d91ce0e
Bhanu Prasad M <mbhanuprsd@gmail.com>
2021-12-14 09:57:52 +0530

    Lecture 118

--- Building_Escape 5.0/Source/Building_Escape/Grabber.cpp
+++ Building_Escape 5.0/Source/Building_Escape/Grabber.cpp
@@ -1,7 +1,10 @@
 // Copyright Bhanu Prasad M 2021
 
-#include "GameFramework/PlayerController.h"
 #include "Grabber.h"
+#include "GameFramework/PlayerController.h"
+#include "DrawDebugHelpers.h"
+
+#define OUT
 
 // Sets default values for this component's properties
 UGrabber::UGrabber()
@@ -13,25 +16,45 @@ UGrabber::UGrabber()
 	// ...
 }
 
-
 // Called when the game starts
 void UGrabber::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
 	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty for %s"), *(GetOwner()->GetActorLabel()))
 }
 
-
 // Called every frame
-void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// Get Players viewpoint
-	
+	FVector ViewPointLocation;
+	FRotator ViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT ViewPointLocation,
+		OUT ViewPointRotation);
+
+	// UE_LOG(LogTemp, Warning, TEXT("Location: %s, Rotation: %s"),
+	// 	*ViewPointLocation.ToString(),
+	// 	*ViewPointRotation.ToString()
+	// );
+
+	// Draw a line from player showing the reach
+	FVector LineTraceEndLocation = ViewPointLocation + ViewPointRotation.Vector() * Reach;
+	DrawDebugLine(
+		GetWorld(),
+		ViewPointLocation,
+		LineTraceEndLocation,
+		FColor(0, 255, 0),
+		true,
+		0.f,
+		0,
+		5.0f);
+
 	// Ray-cast out to certain distance (Reach)
 
 	// See what it hitsS
 }
-
